4c28c5674dec6f200825205eb68f8cd1f8207e88
gregiorg <gergely.fodor@etu.univ-grenoble-alpes.fr>
2020-03-18 10:23:18 +0100

    first commit

---
+++ ftp.c
@@ -0,0 +1,9 @@
+/*
+ * ftp - read a file name and write the content
+ */
+#include "csapp.h"
+
+void ftp(int connfd)
+{
+    
+}
---
+++ ftpserver.c
@@ -0,0 +1,87 @@
+/*
+ * echoserveri.c - An iterative echo server
+ */
+
+#include "csapp.h"
+
+#define MAX_NAME_LEN 256
+#define MAX_NB_FILS 2
+
+void echo(int connfd);
+
+void sigchldHandler(int sig) {
+  int status;
+  waitpid(-1, &status, WNOHANG|WUNTRACED);
+  return;
+}
+
+/*
+ * Note that this code only works with IPv4 addresses
+ * (IPv6 is not supported)
+ */
+int main(int argc, char **argv)
+{
+    Signal(SIGCHLD, sigchldHandler);
+    int listenfd, connfd, port;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+    char client_ip_string[INET_ADDRSTRLEN];
+    char client_hostname[MAX_NAME_LEN];
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+    port = atoi(argv[1]);
+
+    clientlen = (socklen_t)sizeof(clientaddr);
+
+    listenfd = Open_listenfd(port);
+
+
+    for(int i = 0; i < MAX_NB_FILS; i++) {
+      if(!Fork()) {
+        while(1) {
+          connfd = Accept(listenfd, (SA*) &clientaddr, &clientlen);
+
+          /* determine the name of the client */
+          Getnameinfo((SA *) &clientaddr, clientlen,
+                      client_hostname, MAX_NAME_LEN, 0, 0, 0);
+
+          /* determine the textual representation of the client's IP address */
+          Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
+                    INET_ADDRSTRLEN);
+
+          printf("server connected to %s (%s)\n", client_hostname,
+                 client_ip_string);
+
+          echo(connfd);
+          Close(connfd);
+        }
+        exit(0);
+      }
+    }
+
+    for(int i = 0; i < MAX_NB_FILS; i++) {
+      Wait(NULL);
+    }
+
+  printf("All children are dead\n");
+
+    // connfd = Accept(listenfd, (SA*) &clientaddr, &clientlen);
+    //
+    // /* determine the name of the client */
+    // Getnameinfo((SA *) &clientaddr, clientlen,
+    //             client_hostname, MAX_NAME_LEN, 0, 0, 0);
+    //
+    // /* determine the textual representation of the client's IP address */
+    // Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
+    //           INET_ADDRSTRLEN);
+    //
+    // printf("server connected to %s (%s)\n", client_hostname,
+    //        client_ip_string);
+    //
+    // echo(connfd);
+    // Close(connfd);
+    exit(0);
+}
